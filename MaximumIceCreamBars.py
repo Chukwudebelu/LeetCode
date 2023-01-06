5dde09eb9d62c46e2c518f6252d6a200f5ac0662
Chisom Anumba <chisom213@gmail.com>
2023-01-06 09:51:09 -0500

    Create MaximumIceCreamBars.py
    It is a sweltering summer day, and a boy wants to buy some ice cream bars.
    At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
    Return the maximum number of ice cream bars the boy can buy with coins coins.
    Note: The boy can buy the ice cream bars in any order.
    
    Constraints:
    - costs.length == n
    - 1 <= n <= 105
    - 1 <= costs[i] <= 105
    - 1 <= coins <= 108

---
+++ MaximumIceCreamBars.py
@@ -0,0 +1,11 @@
+# 1833. Maximum Ice Cream Bars
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs = sorted(costs)
+        n, ice_cream = len(costs), 0
+
+        while ice_cream < n and costs[ice_cream] <= coins:
+            coins -= costs[ice_cream]
+            ice_cream += 1
+        return ice_cream
