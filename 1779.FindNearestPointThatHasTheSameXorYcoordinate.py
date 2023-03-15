ba71b6496c2541d73d502c3f1a6c3394a111b9ab
Chisom Anumba <chisom213@gmail.com>
2023-03-15 10:57:54 -0400

    1779.FindNearestPointThatHasTheSameXorYcoordinate.py
    You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points [i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
    
    Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
    
    The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
    
    Example 1:
    Input: x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
    Output: 2
    Explanation: Of all the points, only [3,1], [2,4] and [4,4] are valid. Of the valid points, [2,4] and [4,4] have the smallest Manhattan distance from your current location, with a distance of 1. [2,4] has the smallest index, so return 2.
    
    Constraints:
    - 1 <= points.length <= 10^4
    - points[i].length == 2
    - 1 <= x, y, ai, bi <= 10^4

---
+++ 1779.FindNearestPointThatHasTheSameXorYcoordinate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: list[list[int]]) -> int:
+        valid_points = [point for point in points if point[0] == x or point[1] == y]
+        manhattan_dist = lambda a: abs(x - a[0]) + abs(y - a[1])
+        all_valids = list(map(manhattan_dist, valid_points))
+
+        lst = []
+        for j in range(len(valid_points)):
+            if all_valids[j] == min(all_valids):
+                lst += [points.index(valid_points[j])]
+        return min(lst) if lst else -1
