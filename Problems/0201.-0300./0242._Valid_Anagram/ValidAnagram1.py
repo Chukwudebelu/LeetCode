c3576efce0278146259230de1624a596b9225a0c
Chisom Anumba <chisom213@gmail.com>
2023-06-08 08:59:43 -0400

    Create ValidAnagram1.py

---
+++ Problems/0201.-0300./0242._Valid_Anagram/ValidAnagram1.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Use a Counter object to get the frequenices of characters in each string
+        char_count_s = Counter(s)
+        char_count_t = Counter(t)
+
+        return char_count_s == char_count_t
+        
