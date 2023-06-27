f92b7830af343075cc6e61bdc077d342c2f1d1b9
Chisom Anumba <chisom213@gmail.com>
2023-06-27 11:46:45 -0400

    Create SortArrayByParity3.py

---
+++ Problems/0901.-1000./0905._Sort_Array_By_Parity/SortArrayByParity3.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+class Solution:
+    def sortArrayByParity(self, nums: list) -> list:
+        lst = list()
+        i = 0
+      
+        while (i < len(nums)): # Insert all EVEN #s first
+            if (nums[i] % 2 == 0):
+                lst += [nums[i]]
+            i += 1
+        
+        j = 0
+        while (j < len(nums)): # Then, inser all ODD #s
+            if (nums[j] % 2):
+                lst += [nums[j]]
+            j += 1
+        return lst
