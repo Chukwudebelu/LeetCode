0496f76032cb5948d32a6241e4a7f4f57629c483
Chisom Anumba <chisom213@gmail.com>
2023-07-17 10:02:36 -0400

    Create FindtheIndexoftheFirstOccurenceinaString2.py

--- Problems/0001.-0100./0028._Find_the_Index_of_the_First_Occurence_in_a_String/FindtheIndexoftheFirstOccurenceinaString2.py
+++ Problems/0001.-0100./0028._Find_the_Index_of_the_First_Occurence_in_a_String/FindtheIndexoftheFirstOccurenceinaString2.py
@@ -1,10 +1,9 @@
 #!/bin/python3
+
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        n = len(needle)
+  def strStr(self, haystack: str, needle: str) -> int:
+    n = len(needle)
         
-        ind = [i for i in range(len(haystack)-n+1) if haystack[i:i+n] == needle]
+    ind = [i for i in range(len(haystack)-n+1) if haystack[i:i+n] == needle]
         
-        return ind[0] if ind != [] else -1
-            
-        
+    return ind[0] if ind != [] else -1
