ea03d5b4699bd3dd718cdec67f4ddd997cf9f9ed
Chisom Anumba <chisom213@gmail.com>
2024-01-03 12:45:28 -0500

    Create NumberOfLaserBeamsInABank0.py
    Anti-theft security devices are activated inside a bank. You are given a 0-indexed binary string array bank representing the floor plan of the bank, which is an m x n 2D matrix. bank[i] represents the i-th row, consisting of '0's and '1's. '0' means the cell is empty, while'1' means the cell has a security device.
    
    There is one laser beam between any two security devices if both conditions are met:
    
    ∙ The two devices are located on two different rows: r1 and r2, where r1 < r2.
    ∙ For each row i where r1 < i < r2, there are no security devices in the ith row.
    
    Laser beams are independent, i.e., one beam does not interfere nor join with another.
    
    Return the "total number of laser beams in the bank".
    
    Example 1:
    [https://assets.leetcode.com/uploads/2021/12/24/laser1.jpg]
    Input: bank = ["011001","000000","010100","001000"]
    Output: 8
    Explanation: Between each of the following device pairs, there is one beam. In total, there are 8 beams:
     * bank[0][1] -- bank[2][1]
     * bank[0][1] -- bank[2][3]
     * bank[0][2] -- bank[2][1]
     * bank[0][2] -- bank[2][3]
     * bank[0][5] -- bank[2][1]
     * bank[0][5] -- bank[2][3]
     * bank[2][1] -- bank[3][2]
     * bank[2][3] -- bank[3][2]
    Note that there is no beam between any device on the 0th row with any on the 3rd row.
    This is because the 2nd row contains security devices, which breaks the second condition.
    
    Example 2:
    [https://assets.leetcode.com/uploads/2021/12/24/laser2.jpg]
    Input: bank = ["000","111","000"]
    Output: 0
    Explanation: There does not exist two devices located on two different rows.
     
    Constraints:
    ∙ m == bank.length
    ∙ n == bank[i].length
    ∙ 1 <= m, n <= 500
    ∙ bank[i][j] is either '0' or '1'

---
+++ Problems/2101.-2200./2125._Number_of_Laser_Beams_in_a_Bank/NumberOfLaserBeamsInABank0.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        bank_floor = list(map(self.countOne, bank))
+        sec_devices = self.removeZeros(bank_floor)
+
+        laser_beam = 0
+        if len(sec_devices) == 1:
+            return laser_beam
+        else:
+            for i in range(0, len(sec_devices)-1):
+                laser_beam += (sec_devices[i]*sec_devices[i+1])
+        return laser_beam
+
+    # Remove rows with no security device
+    def removeZeros(self, list_num: list) -> list:
+        while 0 in list_num:
+            list_num.remove(0)
+        return list_num
+
+    # Number of security devices
+    def countOne(self, bin_string: str) -> int:
+        return bin_string.count("1")
