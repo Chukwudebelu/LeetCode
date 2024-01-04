3777cf146f7d4967e6cd6b59f06be1ee5484be2a
Chisom Anumba <chisom213@gmail.com>
2024-01-04 17:25:07 -0500

    Create MinNumberOfOperationsToMakeArrayEmpty1.py

---
+++ Problems/2801.-2900./2870._Minimum_Number_of_Operations_to_Make_Array_Empty/MinNumberOfOperationsToMakeArrayEmpty1.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+from collections import Counter
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        num_count, ops = Counter(nums), 0
+
+        for val in num_count.values():
+            if (val == 1):
+                return -1
+            else:
+                ops += math.ceil(val / 3)
+        return ops
