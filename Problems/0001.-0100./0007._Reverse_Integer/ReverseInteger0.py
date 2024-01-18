236c4c092e54fffe7d4bc8585614c6583eb6ce34
Chisom Anumba <chisom213@gmail.com>
2024-01-18 06:10:17 -0500

    Update and rename ReverseInteger.py to Problems/0001.-0100./0007._Reverse_Integer/ReverseInteger0.py
    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2³¹, 2³¹ - 1], then return 0.
    
    Assume the environment does not allow you to store 64-bit integers (signed or unsigned). 
    
    Example 1:
    Input: x = 123
    Output: 321
    
    Example 2:
    Input: x = -123
    Output: -321
    
    Example 3:
    Input: x = 120
    Output: 21 
    
    Constraints:
    • -2³¹ <= x <= 2³¹ - 1

--- Problems/0001.-0100./0007._Reverse_Integer/ReverseInteger0.py
+++ Problems/0001.-0100./0007._Reverse_Integer/ReverseInteger0.py
@@ -1,17 +1,18 @@
 # 7. Reverse Integer
 def reverse(self, x: int) -> int:
-    if -9 <= x <= 9:
+    if (-9 <= x <= 9):
         return x
+        
     str_int = str(x)
-    if str_int[0] == '-':
+    if (str_int[0] == '-'):
         minus = '-'
         str_int = str_int[1:][::-1]
-        while str_int[0] == '0':
+        while (str_int[0] == '0'):
             str_int = str_int[1:]
         str_int = minus + str_int
     else:
         str_int = str_int[::-1]
-        while str_int[0] == '0':
+        while (str_int[0] == '0'):
             str_int = str_int[1:]
-    ans = int(str_int) if -2**31 <= int(str_int) <= 2**31-1 else 0
+    ans = int(str_int) if (-2**31 <= int(str_int) <= 2**31-1) else 0
     return ans
