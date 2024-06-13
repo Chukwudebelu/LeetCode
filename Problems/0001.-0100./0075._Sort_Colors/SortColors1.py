7008ed08d529dfee667f75273a1796f351fd853c
Chisom Anumba <chisom213@gmail.com>
2024-06-13 02:16:15 -0400

    Create SortColors1.py

---
+++ Problems/0001.-0100./0075._Sort_Colors/SortColors1.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+class Solution:
+    def sortColors(self, nums: list) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
