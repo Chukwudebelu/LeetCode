7214dd8ec029da4f980139538157a351cdd5efac
Chisom Anumba <chisom213@gmail.com>
2024-07-10 02:37:38 -0400

    Update and rename OddEvenLinkedLIst0.py to OddEvenLinkedList0.py
    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
    
    The first node is considered odd, and the second node is even, and so on.
    
    Note that the relative order inside both the even and odd groups should remain as it was in the input.
    
    You must solve the problem in O(1) extra space complexity and O(n) time complexity.
    
    Example 1:
    [https://assets.leetcode.com/uploads/2021/03/10/oddeven-linked-list.jpg]
    
      Input: head = [1,2,3,4,5]
      Output: [1,3,5,2,4]
    
    Example 2:
    [https://assets.leetcode.com/uploads/2021/03/10/oddeven2-linked-list.jpg]
    
      Input: head = [2,1,3,5,6,4,7]
      Output: [2,3,6,7,1,5,4]
    
    Constraints:
    • The number of nodes in the linked list is in the range [0, 10⁴].
    • -10⁶ <= Node.val <= 10⁶

--- Problems/0301.-0400./0328._Odd_Even_Linked_List/OddEvenLinkedList0.py
+++ Problems/0301.-0400./0328._Odd_Even_Linked_List/OddEvenLinkedList0.py
@@ -1,13 +1,13 @@
 # 328. Odd Even Linked List
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def oddEvenList(self, head: ListNode) -> ListNode:
         if (not head):
             return head
         else:
