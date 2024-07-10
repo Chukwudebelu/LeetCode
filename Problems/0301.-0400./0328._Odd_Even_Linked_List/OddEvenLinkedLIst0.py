88ce325f268582384e356d49420cbb72ee4d122b
Chisom Anumba <chisom213@gmail.com>
2024-07-10 02:36:25 -0400

    Create OddEvenLinkedList0.py
    Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
    
    The first node is considered odd, and the second node is even, and so on.
    
    Note that the relative order inside both the even and odd groups should remain as it was in the input.
    
    You must solve the problem in O(1) extra space complexity and O(n) time complexity.
    
    Example 1:
    [https://assets.leetcode.com/uploads/2021/03/10/oddeven-linked-list.jpg]
    
      Input: head = [1,2,3,4,5]
      Output: [1,3,5,2,4]
    
    Example 2:
    [https://assets.leetcode.com/uploads/2021/03/10/oddeven2-linked-list.jpg]
    
      Input: head = [2,1,3,5,6,4,7]
      Output: [2,3,6,7,1,5,4]
    
    Constraints:
    • The number of nodes in the linked list is in the range [0, 10⁴].
    • -10⁶ <= Node.val <= 10⁶

---
+++ Problems/0301.-0400./0328._Odd_Even_Linked_List/OddEvenLinkedLIst0.py
@@ -0,0 +1,36 @@
+# 328. Odd Even Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if (not head):
+            return head
+        else:
+            odd_indices, even_indices = [], []
+            curr1, curr2 = head, head
+            ll_size = 0
+
+            while (curr1):  # Get the number of nodes
+                ll_size += 1
+                curr1 = curr1.next
+
+            count = 1   # Tracing index
+            while (curr2 and (count <= ll_size)):
+                if (count % 2 == 1):  # even numbered node
+                    odd_indices += [ListNode(curr2.val)]
+                elif (count % 2 == 0):  # odd numbered node
+                    even_indices += [ListNode(curr2.val)]
+                count += 1
+                curr2 = curr2.next
+
+            odd_indices.extend(even_indices)  # Merge odd & even numbered nodes
+
+            for k in range(len(odd_indices)-1):
+                odd_indices[k].next = odd_indices[k+1]                
+
+            return odd_indices.pop(0)
