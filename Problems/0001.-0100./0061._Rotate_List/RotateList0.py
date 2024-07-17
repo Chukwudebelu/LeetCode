8940ff67dfaf05d8a10f62c11027e4600574bb35
Chisom Anumba <chisom213@gmail.com>
2024-07-17 14:54:02 -0400

    Create RotateList0.py
    Given the head of a linked list, rotate the list to the right by k places.
    
    Example 1:
    [https://assets.leetcode.com/uploads/2020/11/13/rotate1.jpg]
    
      Input: head = [1, 2, 3, 4, 5], k = 2
      Output: [4, 5, 1, 2, 3]
    
    Example 2:
    [https://assets.leetcode.com/uploads/2020/11/13/roate2.jpg]
    
      Input: head = [0, 1, 2], k = 4
      Output: [2, 0, 1]
     
    Constraints:
    • The number of nodes in the list is in the range [0, 500].
    • -100 <= Node.val <= 100
    • 0 <= k <= 2 * 10⁹

---
+++ Problems/0001.-0100./0061._Rotate_List/RotateList0.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+# 61. Rotate List
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if (not head):    # empty linked list
+            return None
+        elif (head and k == 0):    # No rotations!
+            return head
+        else:
+            curr = head
+            length = 0
+            node_vals = list()
+
+            while (curr):
+                node_vals.append(ListNode(curr.val))
+                length += 1
+                curr = curr.next
+
+            k %= length     # get smallest amount of rotations for same results
+
+            new = []
+            for i in range(k):
+                new = [node_vals.pop(-1)] + new
+
+            new += node_vals
+            for j in range(length-1):
+                new[j].next = new[j+1]
+
+            return new[0]
