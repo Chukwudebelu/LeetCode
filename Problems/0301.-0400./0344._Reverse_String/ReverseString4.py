3a214cd33b2e67df9da4a87c2a6aaa1a66343888
Chisom Anumba <chisom213@gmail.com>
2024-07-19 05:06:10 -0400

    Create ReverseString4.py

---
+++ Problems/0301.-0400./0344._Reverse_String/ReverseString4.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s[:] = s[::-1]
