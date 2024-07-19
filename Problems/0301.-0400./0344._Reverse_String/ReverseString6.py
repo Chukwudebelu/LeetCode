01f3239a3d43faef470e4961dc6f65897b571580
Chisom Anumba <chisom213@gmail.com>
2024-07-19 05:09:05 -0400

    Create ReverseString6.py

---
+++ Problems/0301.-0400./0344._Reverse_String/ReverseString6.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+class Solution:
+    def reverseString(self, s: list) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        
